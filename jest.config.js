8f9a06cfedcdc50bfde5f23ef4c8ab9dd390d864
Rutagarama Axcel <rutagaramaaxcel@gmail.com>
2023-04-11 19:23:38 +0200

    boilerplate commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    collectCoverage: true,
+    collectCoverageForm: ['src/**/*.{js,jsx}'],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
