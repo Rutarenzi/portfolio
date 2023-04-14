9f4f9a276ea22ce5e4a5d4df14a38ff37b037429
Rutagarama Axcel <rutagaramaaxcel@gmail.com>
2023-04-14 20:10:10 +0200

    setting up eslint

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes';
 
-
-export default function App(){
-    return(
-      <Router>
-        <AppRoutes />
-      </Router>
-    );
+export default function App() {
+  return (
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
 }
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-    <App />
-);
-
+root.render(<App />);
